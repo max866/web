85ab48ace1889edeec0a84d0eb079b29359c3e02
max866 <max866.hung@gmail.com>
2019-08-13 13:56:33 +0800

    add accounting book functions frontend develop

---
+++ getAccountBookJS.js
@@ -0,0 +1,18 @@
+$.ajax({
+  success: function() {
+    // 給input  date設定預設值
+    var now = new Date();
+
+    //格式化日，如果小於9，前面補0
+    var day = ("0" + now.getDate()).slice(-2);
+
+    //格式化月，如果小於9，前面補0
+    var month = ("0" + (now.getMonth() + 1)).slice(-2);
+
+    //拼裝完整日期格式
+    var today = now.getFullYear() + "-" + (month) + "-" + (day);
+
+    //完成賦值
+    $('#calendar').val(today);
+  }
+});
